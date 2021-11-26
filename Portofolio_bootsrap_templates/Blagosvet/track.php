febc2d04cf4e7bb3fccbe3b92f25d8bb93af59ca
Account931 <account931@ukr.net>
2021-11-26 13:02:14 +0300

    Doc(): added ZIP templates

---
+++ Portofolio_bootsrap_templates/Blagosvet/track.php
@@ -0,0 +1,8 @@
+
+<?php
+// Record (with CLASS) all the  input  to  txt;  //;
+      include("Classes/RecordTxt.php");
+      RecordTxt::RecordAnyInput(array(), 'recordText/myWazeCoords.txt');// Record  to  text;
+//End  Record;
+
+?>
